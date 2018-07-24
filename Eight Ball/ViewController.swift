4d1734ff56e7594d867e2018a63c75c5707f3a5d
Samantha Gatt <smfgatt@gmail.com>
2018-07-24 12:59:49 -0400

    Initial commit

---
+++ Eight Ball/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Eight Ball
+//
+//  Created by Samantha Gatt on 7/24/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func flipEightBall(_ sender: Any) {
+        
+        let index = Int(arc4random_uniform(UInt32(answers.count)))
+        let answer = answers[index]
+        answerLabel.text = answer
+    }
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    private let answers = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no",
+        "Outlook not so good.",
+        "Very doubtful.",
+        ]
+
+}
+
